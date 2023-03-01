e34c2e15bb6c95a8c8c815330d2bcbdd567bc538
dsofienko4 <dsofienko1@gmail.com>
2023-03-01 16:19:35 +0300

    commit_1

---
+++ src/main/java/App.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getName());
+    }
+}
